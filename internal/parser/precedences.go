326848ed04f5aad8954208a54bd0d8eb036b8b11
OJOMB <oscarm-b@tutamail.com>
2026-04-06 15:24:01 +0200

    move precedences to separate file

---
+++ internal/parser/precedences.go
@@ -0,0 +1,35 @@
+package parser
+
+import "github.com/OJOMB/donkey/internal/tokens"
+
+const (
+	// Precedence levels for parsing expressions
+
+	// precedenceLowest is the lowest precedence level, used for expressions that don't have any operators.
+	precedenceLowest int = iota
+	// precedenceEquals is the precedence level for the equality operator (==).
+	precedenceEquals
+	// precedenceLessGreater is the precedence level for the less than and greater than operators (< and >).
+	precedenceLessGreater
+	// precedenceAdditive is the precedence level for addition and subtraction operators (+ and -).
+	precedenceAdditive
+	// precedenceMultiplicative is the precedence level for multiplication and division operators (* and /).
+	precedenceMultiplicative
+	// precedencePrefix is the precedence level for prefix operators, such as -X or !X.
+	precedencePrefix
+	// precedenceCall is the precedence level for function call expressions, such as myFunction(X).
+	precedenceCall
+)
+
+var precedences = map[tokens.Type]int{
+	tokens.TypeEq:           precedenceEquals,
+	tokens.TypeNotEq:        precedenceEquals,
+	tokens.TypeLT:           precedenceLessGreater,
+	tokens.TypeGT:           precedenceLessGreater,
+	tokens.TypeBang:         precedencePrefix,
+	tokens.TypePlus:         precedenceAdditive,
+	tokens.TypeMinus:        precedenceAdditive,
+	tokens.TypeForwardSlash: precedenceMultiplicative,
+	tokens.TypeAsterisk:     precedenceMultiplicative,
+	tokens.TypeLParen:       precedenceCall,
+}
