bcea924dc2cdd0ffff057eb73bd4d0ffd9af4ae8
OJOMB <oscarm-b@tutamail.com>
2026-04-06 15:33:36 +0200

    remove ctx logging

--- pkg/logs/logger.go
+++ pkg/logs/logger.go
@@ -1,15 +1,11 @@
 package logs
 
-import (
-	"context"
-)
-
 type Logger interface {
-	DebugContext(ctx context.Context, msg string, args ...any)
-	ErrorContext(ctx context.Context, msg string, args ...any)
-	InfoContext(ctx context.Context, msg string, args ...any)
-	WarnContext(ctx context.Context, msg string, args ...any)
-	FatalContext(ctx context.Context, msg string, args ...any)
-	Log(ctx context.Context, level Level, msg string, args ...any)
+	Debug(msg string, args ...any)
+	Error(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Fatal(msg string, args ...any)
+	Log(level Level, msg string, args ...any)
 	With(args ...any) Logger
 }
--- pkg/logs/slogger.go
+++ pkg/logs/slogger.go
@@ -36,9 +36,9 @@ func NewMultiSlogger(handlers ...slog.Handler) Logger {
 	return &Slogger{slog.New(slog.NewMultiHandler(handlers...))}
 }
 
-// NewNullSlogger returns a logger that discards all log messages.
+// NewNullLogger returns a logger that discards all log messages.
 // This can be used as a default logger to avoid nil pointer dereference when a logger is not provided.
-func NewNullSlogger() Logger {
+func NewNullLogger() Logger {
 	return &Slogger{slog.New(slog.NewJSONHandler(io.Discard, nil))}
 }
 
@@ -47,34 +47,34 @@ func (sl *Slogger) With(args ...any) Logger {
 	return &Slogger{sl.Logger.With(args...)}
 }
 
-// InfoContext logs a message at the Info level with the provided context and key-value pairs.
-func (sl *Slogger) InfoContext(ctx context.Context, msg string, args ...any) {
-	sl.Logger.InfoContext(ctx, msg, args...)
+// Info logs a message at the Info level with the provided key-value pairs.
+func (sl *Slogger) Info(msg string, args ...any) {
+	sl.Logger.Info(msg, args...)
 }
 
-// DebugContext logs a message at the Debug level with the provided context and key-value pairs.
-func (sl *Slogger) DebugContext(ctx context.Context, msg string, args ...any) {
-	sl.Logger.DebugContext(ctx, msg, args...)
+// Debug logs a message at the Debug level with the provided key-value pairs.
+func (sl *Slogger) Debug(msg string, args ...any) {
+	sl.Logger.Debug(msg, args...)
 }
 
-// WarnContext logs a message at the Warn level with the provided context and key-value pairs.
-func (sl *Slogger) WarnContext(ctx context.Context, msg string, args ...any) {
-	sl.Logger.WarnContext(ctx, msg, args...)
+// Warn logs a message at the Warn level with the provided key-value pairs.
+func (sl *Slogger) Warn(msg string, args ...any) {
+	sl.Logger.Warn(msg, args...)
 }
 
-// ErrorContext logs a message at the Error level with the provided context and key-value pairs.
-func (sl *Slogger) ErrorContext(ctx context.Context, msg string, args ...any) {
-	sl.Logger.ErrorContext(ctx, msg, args...)
+// Error logs a message at the Error level with the provided key-value pairs.
+func (sl *Slogger) Error(msg string, args ...any) {
+	sl.Logger.Error(msg, args...)
 }
 
-// Log logs a message at the specified level with the provided context and key-value pairs.
-func (sl *Slogger) Log(ctx context.Context, level Level, msg string, args ...any) {
-	sl.Logger.Log(ctx, slog.Level(level), msg, args...)
+// Log logs a message at the specified level with the provided key-value pairs.
+func (sl *Slogger) Log(level Level, msg string, args ...any) {
+	sl.Logger.Log(context.Background(), slog.Level(level), msg, args...)
 }
 
-// FatalContext logs a message at the Fatal level with the provided context and key-value pairs, and then exits the application.
-func (sl *Slogger) FatalContext(ctx context.Context, msg string, args ...any) {
-	sl.Logger.Log(ctx, slog.LevelError, msg, args...)
+// Fatal logs a message at the Fatal level with the provided key-value pairs, and then exits the application.
+func (sl *Slogger) Fatal(msg string, args ...any) {
+	sl.Logger.Error(msg, args...)
 
 	// TODO: slog does not have Fatal level, so we exit the application here
 	// I'm not certain this gives enough time for the logs on both handlers to be flushed?
