d0de09d07d314b09c08bf05d1dd03d0443f2b579
OJOMB <oscarm-b@tutamail.com>
2026-06-29 20:07:06 +0200

    add start of bytecode

---
+++ internal/bytecode/code.go
@@ -0,0 +1,23 @@
+package bytecode
+
+type Instructions []byte
+
+type Opcode byte
+
+const (
+	OpConstant Opcode = iota
+)
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op Opcode) (*Definition, bool) {
+	def, ok := definitions[op]
+	return def, ok
+}
